44bbe2af43d9fd2aafdb6417d5113ef491aa4bb5
shailender <shailenderg7@gmail.com>
2022-07-07 16:12:42 +0530

    change in whole class

--- seo_app/image_scrapping.py
+++ seo_app/image_scrapping.py
@@ -1,67 +1,53 @@
-from bs4 import BeautifulSoup as soup
+from attr import attr
+from bs4 import BeautifulSoup 
 import requests
 
 
 class ScrappedImage:
+    def __init__(self, url, img_source=None, alt_text=None):
+       self.url = url
+       self.img_source = img_source
+       self.alt_text = alt_text
 
-    def __init__(self,alt_text, url_source):
-        self.url_source = url_source
-        self.alt_text = alt_text
 
+    def get_image_attributes(self):
+       reqs = requests.get(self.url)
+       soup = BeautifulSoup(reqs.text, 'html.parser')
+       
+       image_tag = soup.find('div', class_="banner-img").find('img')          
+       img_source =  image_tag['src']
+       alt_text =   image_tag['alt']
+       lis = [img_source,alt_text]
+       return (lis)    
+
+    def get_img_source(self):
+       reqs = requests.get(self.url)
+       soup = BeautifulSoup(reqs.text, 'html.parser')
+       
+       image_tag = soup.find('div', class_="banner-img").find('img')          
+       img_source =  image_tag['src']
+       return img_source
+
+    def get_alt_text(self):
+       reqs = requests.get(self.url)
+       soup = BeautifulSoup(reqs.text, 'html.parser')
+       
+       image_tag = soup.find('div', class_="banner-img").find('img')          
+       alt_text =   image_tag['alt']
+       return alt_text    
 
     def has_alt_text(self):
-               
         '''Return a bool value where True is returned,
-        if there is an alt text present for the given image.
+           if there is an alt text present for the given image.
         '''       
-        
-        return bool(self.alt_text)        
-
+        alt_text = self.get_alt_text()
+        return bool(alt_text[1])        
 
     def to_representation(self):
-
+        '''It will return the dictionary containing image source and alt text'''
         dict = {
-            "url_source": self.url_source ,
-            "alt_text": self.alt_text
+            "img_source": self.get_img_source(),
+            "alt_text": self.get_alt_text(),
         }
-
-        return dict
-        
-
-img_source = ""
-alt_text = ""
-def get_alt_text(url):
-
-    reqs = requests.get(url)
-    soup1 = soup(reqs.text, 'html.parser')
-    
-    con = soup1.find('div', class_="banner-img").find('img')
-    conn = '<img alt="GEEK-O-LYMPICS 2022 - May The Geeks Force Be With You!" src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220630162102/GEEK-O-LYMPICS-2022-May-The-Geeks-Force-Be-With-You.png"/>'
-        
-    soup1 = soup(conn,
-                features="html.parser"
-            )
-
-    img_source = soup1.find("img")['src']
-    alt_text = soup1.find("img")["alt"]
-    lis = [img_source,alt_text]
-    return (lis)
-   
-   
-def main():
-
-    url = 'https://www.geeksforgeeks.org/'
-    ab = get_alt_text(url)
-    alt_text = ab[0]
-    img_source = ab[1]
-
-    obj = ScrappedImage(alt_text,img_source)
-    print(obj.to_representation())
-
-   
-if __name__ == "__main__":
-    main()    
-        
-
-
+        return dict       
 
